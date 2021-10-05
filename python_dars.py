6c0b78566a0ca1fce8fc7fb009448f4de6dc79c3
eraliabd <91982815+eraliabd@users.noreply.github.com>
2021-10-06 00:41:06 +0500

    fayl yangilandi

--- python_dars.py
+++ python_dars.py
@@ -2,10 +2,11 @@
 """
 Created on Wed Oct  6 00:25:54 2021
 
-@author: DELL
+@author: DELL Erali Abdinazarov
 """
 
 print("Assalomu alaykum")
+
 dasturlash = input("Qaysi dasturlash tilini o'rganasiz?\n>>>")
 if dasturlash == "Python":
     print("Sizga Omad!")
