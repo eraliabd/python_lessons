6d56fc5fcd654c2ad86b2576f207329edea29978
eraliabd <91982815+eraliabd@users.noreply.github.com>
2021-10-06 00:37:46 +0500

    yangi fayl qo'shildi
    Mashq qo'shildi

---
+++ python_dars.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  6 00:25:54 2021
+
+@author: DELL
+"""
+
+print("Assalomu alaykum")
+dasturlash = input("Qaysi dasturlash tilini o'rganasiz?\n>>>")
+if dasturlash == "Python":
+    print("Sizga Omad!")
+else:
+    print("Qaysi dasturlash tilini o'rganishigizdan qat'i nazar sizga Omad!")
