d025dcb184fb6f6239a6aa689e77b27872c0e37d
eraliabd <91982815+eraliabd@users.noreply.github.com>
2021-10-10 00:27:00 +0500

    Yangi fayl jaylandi
    masala

---
+++ masala/python_dars.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  10 00:25:54 2021
+
+@author: DELL Erali Abdinazarov
+"""
+
+s = input("Satr kiriting: ")
+unli_hariflar = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+undosh_hariflar = ['b', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'z', "g'", 'sh', 'ch', 'ng']
+for x in s:
+    if x == s[0]:
+        if x.lower() in undosh_hariflar:
+            print(x, "undosh harifi bilan boshlangan")
+        else:
+            print(x, 'bunday undosh harifi yo\'q')
+for y in s:
+    if y == s[-1]:
+        if y in unli_hariflar:
+            print(y, "unli harifi bilan tugagan")
+        else:
+          print(y, 'bunday unli harifi yo\'q') 
--- python_dars.py
+++ python_dars.py
@@ -5,10 +5,3 @@ Created on Wed Oct  6 00:25:54 2021
 @author: DELL Erali Abdinazarov
 """
 
-print("Assalomu alaykum")
-
-dasturlash = input("Qaysi dasturlash tilini o'rganasiz?\n>>>")
-if dasturlash == "Python":
-    print("Sizga Omad!")
-else:
-    print("Qaysi dasturlash tilini o'rganishigizdan qat'i nazar sizga Omad!")
