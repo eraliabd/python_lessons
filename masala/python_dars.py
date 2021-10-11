2e4fab1f2272ab8710b04fa4b7a1249d5820cb48
eraliabd <91982815+eraliabd@users.noreply.github.com>
2021-10-11 14:53:39 +0500

    Masala faylimiz yangilandi

--- masala/python_dars.py
+++ masala/python_dars.py
@@ -4,20 +4,21 @@ Created on Sun Oct  10 00:25:54 2021
 
 @author: DELL Erali Abdinazarov
 """
-# Masala sharti
-#s satr berilgan .Satrni oxirgi harfi unli va birinchi harfi undosh harfi bilan boshlanganini tekshiring.
+
 s = input("Satr kiriting: ")
 unli_hariflar = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
 undosh_hariflar = ['b', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'z', "g'", 'sh', 'ch', 'ng']
 for x in s:
-    if x == s[0]:
-        if x.lower() in undosh_hariflar:
-            print(x, "undosh harifi bilan boshlangan")
-        else:
-            print(x, 'bunday undosh harifi yo\'q')
+    if x == s[1]:
+        break
+    if x.lower() in undosh_hariflar:
+        print(x, "undosh harifi bilan boshlangan")
+    else:
+        print(x, 'bunday undosh harifi yo\'q')
 for y in s:
     if y == s[-1]:
         if y in unli_hariflar:
             print(y, "unli harifi bilan tugagan")
+            break
         else:
-          print(y, 'bunday unli harifi yo\'q') 
+            print(y, 'bunday unli harifi yo\'q')  
