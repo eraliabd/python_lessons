e45fae0941481bb1253e2a2f60bf34f9dc9a6249
Erali Abdinazarov <91982815+eraliabd@users.noreply.github.com>
2021-10-12 09:54:56 +0500

    Update python_dars.py

--- python_dars.py
+++ python_dars.py
@@ -5,3 +5,10 @@ Created on Wed Oct  6 00:25:54 2021
 @author: DELL Erali Abdinazarov
 """
 
+print("Assalomu alaykum")
+
+dasturlash = input("Qaysi dasturlash tilini o'rganasiz?\n>>>")
+if dasturlash == "Python":
+    print("Sizga Omad!")
+else:
+    print("Qaysi dasturlash tilini o'rganishigizdan qat'i nazar sizga Omad!")
